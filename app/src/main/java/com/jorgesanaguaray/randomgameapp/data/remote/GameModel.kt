3a4081f58d884c7e63a0e4c9cc8b4a25a633289b
Jorge Sanaguaray <jorgesanaguaray1@gmail.com>
2022-10-11 09:15:02 -0500

    I have created the GameModel data class.

---
+++ app/src/main/java/com/jorgesanaguaray/randomgameapp/data/remote/GameModel.kt
@@ -0,0 +1,15 @@
+package com.jorgesanaguaray.randomgameapp.data.remote
+
+/**
+ * Created by Jorge Sanaguaray
+ */
+
+data class GameModel(
+
+    val id: Int?,
+    val title: String?,
+    val thumbnail: String?,
+    val short_description: String?,
+    val game_url: String?
+
+)
