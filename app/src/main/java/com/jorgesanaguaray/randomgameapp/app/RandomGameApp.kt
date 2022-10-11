0d39012858bc260de61c9f3588599c4fcc728a1f
Jorge Sanaguaray <jorgesanaguaray1@gmail.com>
2022-10-11 09:21:23 -0500

    I have created the RandomGameApp class.

---
+++ app/src/main/java/com/jorgesanaguaray/randomgameapp/app/RandomGameApp.kt
@@ -0,0 +1,11 @@
+package com.jorgesanaguaray.randomgameapp.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by Jorge Sanaguaray
+ */
+
+@HiltAndroidApp
+class RandomGameApp : Application()
