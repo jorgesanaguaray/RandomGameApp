56bdf34c5369b0b353fe397aa4a3837aee03856a
Jorge Sanaguaray <jorgesanaguaray1@gmail.com>
2022-10-11 09:11:00 -0500

    I have created the Constants class.

---
+++ app/src/main/java/com/jorgesanaguaray/randomgameapp/util/Constants.kt
@@ -0,0 +1,17 @@
+package com.jorgesanaguaray.randomgameapp.util
+
+/**
+ * Created by Jorge Sanaguaray
+ */
+
+class Constants {
+
+    companion object {
+
+        const val BASE_URL = "https://www.freetogame.com/api/"
+        const val GAMES = "games"
+        const val DATABASE_NAME = "GameDatabase"
+
+    }
+
+}
